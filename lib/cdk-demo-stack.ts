ccad843f352f344f18202fba1901f4a035812d5f
Joe Devine <joedevine@uk.ibm.com>
2021-08-19 15:52:05 +0100

    Update to use api gateway and lambda

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,42 +1,37 @@
-import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-// import * as msk from '@aws-cdk/aws-msk';
-import * as ec2 from '@aws-cdk/aws-ec2';
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as dotenv from 'dotenv';
 
-// vpc id: vpc-0feb1388aa8e6f3df
-// kafka-m5-large
-// storage volume per broker 100
+dotenv.config();
 
+const { BASE_NAME, LAMBDA_ROLE_NAME = '', LAMBDA_ROLE_ARN = '' } = process.env;
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'TestCDKBucket', {
+    new s3.Bucket(this, `${BASE_NAME}Bucket`, {
       versioned: true,
+    });
+
+    const lambdaRole = iam.Role.fromRoleArn(
+      this,
+      LAMBDA_ROLE_NAME,
+      LAMBDA_ROLE_ARN,
+    );
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'example.handler',
+      role: lambdaRole,
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
+      cloudWatchRole: false,
     });
   }
 }
-
-// const cluster = new msk.Cluster(this, 'Cluster', {
-//   kafkaVersion: msk.KafkaVersion.V2_6_2,
-//   bootstrapBrokers: '',
-//   vpc,
-// });
-
-// cluster.connections.allowFrom(ec2.Peer.ipv4('1.2.3.4/8'), ec2.Port.tcp(2181));
-
-// new cdk.CfnOutput(this, 'BootstrapBrokers', {
-//   value: cluster.bootstrapBrokers,
-// });
-// new cdk.CfnOutput(this, 'BootstrapBrokersTls', {
-//   value: cluster.bootstrapBrokersTls,
-// });
-// new cdk.CfnOutput(this, 'BootstrapBrokersSaslScram', {
-//   value: cluster.bootstrapBrokersSaslScram,
-// });
-// new cdk.CfnOutput(this, 'ZookeeperConnection', {
-//   value: cluster.zookeeperConnectionString,
-// });
-// new cdk.CfnOutput(this, 'ZookeeperConnectionTls', {
-//   value: cluster.zookeeperConnectionStringTls,
-// });
